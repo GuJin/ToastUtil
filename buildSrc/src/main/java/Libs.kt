58a9066c6b1b905ccd0a2c42808656737642a005
gujin <gujin@gujin.com>
2022-08-23 10:36:25 +0800

    gradle upgrade

---
+++ buildSrc/src/main/java/Libs.kt
@@ -0,0 +1,6 @@
+object Androidx {
+
+    const val startup = "androidx.startup:startup-runtime:${Versions.androidx_startup}"
+    const val annotation = "androidx.annotation:annotation:${Versions.androidx_annotation}"
+
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,9 @@
+object Versions {
+
+    const val compileSdkVersion = 31
+    const val targetSdkVersion = 31
+
+    const val androidx_startup = "1.1.1"
+    const val androidx_annotation = "1.4.0"
+
+}
